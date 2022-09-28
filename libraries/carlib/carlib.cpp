b526e3be75e96ef88912b4a3e1fab90bf84ceecc
Boris Kurtis Bignumb-bi-Kumb <1612729+oneshepherdssheep@users.noreply.github.com>
2022-09-28 13:24:38 +0100

    Activate PVS-Studio free analysis in all sub-projects

--- libraries/carlib/carlib.cpp
+++ libraries/carlib/carlib.cpp
@@ -1,3 +1,6 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 //
 // Created by oneshepherdssheep on 25/08/22.
 //
